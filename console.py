54ecc245842bd9aacd8bfa1a548d8e6f1c822924
HAPIBANKS <posiayo1@gmail.com>
2024-01-15 00:16:54 +0100

    console

--- console.py
+++ console.py
@@ -123,7 +123,7 @@ class HBNBCommand(cmd.Cmd):
         print("]")
 
     def do_update(self, arg):
-        """Update an instance based on the class name, id, attribute & value"""
+        """Update an instance based on the class name, id, attributes & value"""
         args = shlex.split(arg)
         integers = ["number_rooms", "number_bathrooms", "max_guest",
                     "price_by_night"]
